92fc84e3e1b53fa1990f94594c79b7fcc2992d4b
trista-paul <44633004+trista-paul@users.noreply.github.com>
2019-03-22 08:38:51 -0700

    Create Product class

---
+++ acme.py
@@ -0,0 +1,16 @@
+"""
+models the products at Acme corp.
+"""
+
+"""
+imports
+"""
+import random
+
+
+class Product:
+    def __init__(self, name, price=10, weight=20, flammability=0.5,
+                 id=random.randint(1000000, 9999999)):
+        self.tools = tools
+        
+   
